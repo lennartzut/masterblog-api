953c16e3c561a831f1afafedda47366657bc9b09
Leo-Zeneo <78357307+Leo-Zeneo@users.noreply.github.com>
2024-09-22 16:11:08 +0200

    Add first endpoint 'add' to create new post

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -14,6 +14,25 @@ POSTS = [
 def get_posts():
     return jsonify(POSTS)
 
+
+@app.route('/api/posts', method=['POST'])
+def add_post():
+    data = request.get_json()
+
+    if not data or 'title' not in data or 'content' not in data:
+        return jsonify({"error": "Both title and content are "
+                                 "mandatory"}), 400
+
+    new_id = max(post['id'] for post in POSTS) + 1 if POSTS else 1
+
+    new_post = {
+        "id": new_id,
+        "title": data['title'],
+        "content": data['content']
+    }
+
+    POSTS.append(new_post)
+    return jsonify(new_post), 201
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5002, debug=True)
