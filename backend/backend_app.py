6eb7367ee9dfd5bfc6e7e1de66022351f12c4296
Leo-Zeneo <78357307+Leo-Zeneo@users.noreply.github.com>
2024-09-22 16:30:40 +0200

    Add DELETE route to backend_app

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -15,24 +15,31 @@ def get_posts():
     return jsonify(POSTS)
 
 
-@app.route('/api/posts', method=['POST'])
+@app.route('/api/posts', methods=['POST'])
 def add_post():
     data = request.get_json()
-
     if not data or 'title' not in data or 'content' not in data:
         return jsonify({"error": "Both title and content are "
                                  "mandatory"}), 400
-
     new_id = max(post['id'] for post in POSTS) + 1 if POSTS else 1
-
     new_post = {
         "id": new_id,
         "title": data['title'],
         "content": data['content']
     }
-
     POSTS.append(new_post)
     return jsonify(new_post), 201
+
+
+@app.route('/api/posts/<int:id>', methods=['DELETE'])
+def delete_post(id):
+    post = next((post for post in POSTS if post["id"] == id), None)
+    if post is None:
+        return jsonify({"error": f"Post id {id} not found"}), 404
+    POSTS.remove(post)
+    return jsonify({"message": f"Post with id {id} has been "
+                               f"deleted"})
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5002, debug=True)
